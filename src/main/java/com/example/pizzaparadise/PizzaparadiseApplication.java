fbb93fa101be0355ebc88f248ea4bd1e2c6a8fd1
Tino <larsen2099@hotmail.com>
2025-03-26 09:05:35 +0100

    Rigtig paradise

---
+++ src/main/java/com/example/pizzaparadise/PizzaparadiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzaparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaparadiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaparadiseApplication.class, args);
+    }
+
+}
